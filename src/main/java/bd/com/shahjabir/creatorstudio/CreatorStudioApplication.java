9843503dbf4bc5a2ea5c5feee8dca347665766d7
ShahJabir <shahjabirofficial@gmail.com>
2026-05-22 00:14:35 +0600

    init: init project

---
+++ src/main/java/bd/com/shahjabir/creatorstudio/CreatorStudioApplication.java
@@ -0,0 +1,13 @@
+package bd.com.shahjabir.creatorstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatorStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bd/com/shahjabir/creatorstudio/CreatorStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package bd.com.shahjabir.creatorstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
