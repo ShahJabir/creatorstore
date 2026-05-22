63b4c135e0b773e09ac3ffae7d4b4815a3a40796
ShahJabir <shahjabirofficial@gmail.com>
2026-05-22 19:54:23 +0600

    feat: .env added

--- src/main/java/bd/com/shahjabir/creatorstudio/CreatorStudioApplication.java
+++ src/main/java/bd/com/shahjabir/creatorstudio/CreatorStudioApplication.java
@@ -1,5 +1,6 @@
 package bd.com.shahjabir.creatorstudio;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreatorStudioApplication {
 
     public static void main(String[] args) {
+//        Configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry) ->
+                System.setProperty(
+                        entry.getKey(), entry.getValue()
+                ));
         SpringApplication.run(CreatorStudioApplication.class, args);
     }
 
